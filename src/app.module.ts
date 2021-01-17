10de60855032da3675f62986198404779add1576
Antti Lappalainen <anttidroid@gmail.com>
2021-01-17 20:10:05 +0200

    Controllers, Services and DTOs imported into modulte

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,11 @@
+import { ConfigModule } from '@nestjs/config';
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { UserModule } from './user/user.module';
 
 @Module({
-  imports: [],
+  imports: [UserModule, ConfigModule.forRoot()],
   controllers: [AppController],
   providers: [AppService],
 })
